a74e7672f48091d0dff7abf4c6f07813d16c1f72
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-21 12:06:20 +0530

    file changed

--- main.js
+++ main.js
@@ -2,6 +2,21 @@ const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+
+  fs.readFile(filePath, 'utf-8', (err, data)=>{
+    if(err){
+      console.error(`Error reading file: ${err.message}`, filePath);
+      return;
+    }
+    console.log(data.trim());
+  })
 }
 
 // TODO: Call printFileContents with the command-line argument
+const filePath = process.argv[2];
+
+if(!filePath){
+  console.log('Please provide a file path');
+  process.exit(1);
+}
+printFileContents(filePath);
